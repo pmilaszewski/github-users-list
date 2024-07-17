b28bd4a548d00ea474936c54d726e50d5bb43607
Pawel Milaszewski <pmilaszewski@apptension.com>
2024-07-17 15:48:20 +0200

    add prettier and eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { Main } from './screens'
-import 'react-native-reanimated';
+import 'react-native-reanimated'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  })
 
   useEffect(() => {
     if (loaded) {
-      SplashScreen.hideAsync();
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [loaded])
 
   if (!loaded) {
-    return null;
+    return null
   }
 
   return (
@@ -29,5 +29,5 @@ export default function RootLayout() {
       <StatusBar style="dark" />
       <Main />
     </SafeAreaView>
-  );
+  )
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,11 +1,11 @@
-const white = "rgba(255,255,255,1)"
-const black = "rgba(0,0,0,1)"
+const white = 'rgba(255,255,255,1)'
+const black = 'rgba(0,0,0,1)'
 
 export const Colors = {
   text: black,
-  subtext: "rgba(64,64,64,1)",
+  subtext: 'rgba(64,64,64,1)',
   background: white,
-  buttonBackground: "rgba(89,149,216,1)",
+  buttonBackground: 'rgba(89,149,216,1)',
   buttonText: white,
   icon: black,
-};
+}
