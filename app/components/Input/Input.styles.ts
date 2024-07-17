0a82fc73ef5d19857c9b0560bcbcedf066df566c
Pawel Milaszewski <pmilaszewski@apptension.com>
2024-07-17 17:17:23 +0200

    Husky + input

--- app/components/Input/Input.styles.ts
+++ app/components/Input/Input.styles.ts
@@ -1,3 +1,9 @@
 import { StyleSheet } from 'react-native'
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 16,
+    paddingHorizontal: 4,
+  }
+})
