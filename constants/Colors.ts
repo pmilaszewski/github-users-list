86c434ad6c3203fba15670f10341abcabe7873a3
Pawel Milaszewski <pmilaszewski@apptension.com>
2024-07-17 15:30:24 +0200

    update colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,11 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const white = "rgba(255,255,255,1)"
+const black = "rgba(0,0,0,1)"
 
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  text: black,
+  subtext: "rgba(64,64,64,1)",
+  background: white,
+  buttonBackground: "rgba(89,149,216,1)",
+  buttonText: white,
+  icon: black,
 };
