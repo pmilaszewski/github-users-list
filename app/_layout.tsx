2fc242ccefa3c04467d855f7a5efa291c670a68b
Pawel Milaszewski <pmilaszewski@apptension.com>
2024-07-17 15:59:09 +0200

    add husky

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,12 +4,15 @@ import * as SplashScreen from 'expo-splash-screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { Main } from './screens'
+
+
 import 'react-native-reanimated'
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -26,8 +29,7 @@ export default function RootLayout() {
 
   return (
     <SafeAreaView>
-      <StatusBar style="dark" />
-      <Main />
+      <StatusBar style="dark" /><Main />
     </SafeAreaView>
   )
 }
