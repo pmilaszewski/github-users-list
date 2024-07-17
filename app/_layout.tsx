7dee436937ee3967bfadf77e5a961fe4a6eb4c23
Pawel Milaszewski <pmilaszewski@apptension.com>
2024-07-17 18:01:46 +0200

    remove splash screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,9 @@
-import * as SplashScreen from 'expo-splash-screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { Main } from './screens'
 import { Layout } from './components'
 
-import 'react-native-reanimated'
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync()
-
 export default function RootLayout() {
-
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="dark" />
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,10 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
 ]
